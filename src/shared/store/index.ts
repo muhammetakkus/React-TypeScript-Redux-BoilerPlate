ba5de51a34e5fac6d7aac6b754b55a77e01efe18
Muhammet Akkus <muhammetakkuss34@gmail.com>
2022-10-05 15:12:15 +0300

    Update index.ts

--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -1,15 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
-import { useDispatch } from 'react-redux'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 /* Reducerlar Component Klasörlerinin İçinde veya reducers klasöründe */
 import auth from '../../components/features/Auth/auth.reducer'
-import role from '../../components/features/AuthSettings/Role/role.reducer'
 import reduxThunkMiddleware from '../utils/redux-thunk.middleware'
 
 const store = configureStore({
     reducer: {
         auth,
-        role
     },
     middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
@@ -18,7 +16,9 @@ const store = configureStore({
 })
 
 export type AppDispatch = typeof store.dispatch
+export type IRootState = ReturnType<typeof store.getState>;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<IRootState> = useSelector;
 
 export default store
